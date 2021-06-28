5f6c8e5b3a4c1eb9a7abfc8bce29ddb70287d661
jojoldu <jojoldu@gmail.com>
2021-06-29 08:25:04 +0900

    project setting

---
+++ jest.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    moduleFileExtensions: ['js', 'json', 'ts'],
+    testPathIgnorePatterns: [
+        "<rootDir>/node_modules/"
+    ],
+    testMatch: [
+        '<rootDir>/test/**/*.test.ts',
+    ],
+    transform: {
+        '^.+\\.(t|j)s$': 'ts-jest',
+    },
+    collectCoverageFrom: [
+        'src/**/*.ts',
+        '!src/app.ts',
+        '!src/start.ts',
+        '!src/**/*.config.ts',
+        '!src/**/entity/**',
+        '!src/**/exception/**',
+    ],
+    coverageDirectory: 'coverage',
+    testEnvironment: 'node'
+};
